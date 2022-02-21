4b01ff650322bed266e4c345bf1902a4f4cf1054
Vest1ge <Vest1ge@users.noreply.github.com>
2022-02-21 15:10:53 +0900

    타이틀 화면 제작, UI 틀(프로토타입)
    타이틀 화면 제작, UI 틀(프로토타입)

---
+++ Defense/Assets/Scripts/StartClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartClick : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
