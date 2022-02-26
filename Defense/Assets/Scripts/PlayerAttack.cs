faf5809018ae9ca154716ade839dca62163eb87d
KwonJeHan <79437053+KwonJeHan@users.noreply.github.com>
2022-02-27 07:48:57 +0900

    키보드 휘두르기 구현 (엉망)
    Weapon 스크립트, PlayerAttack 스크립트 구현
    키보드에 Effect 자식 오브젝트 추가
    키보드에 BoxCollider 컴포넌트 추가

---
+++ Defense/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private Animator animator;
+    public Weapon weapon;
+    private float fireDelay;
+    private bool isFireReady;
+
+    private void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerInput.fire == true)
+        {
+            Attack();
+        }
+    }
+    
+    public void Attack()
+    {
+        fireDelay += Time.deltaTime;
+        isFireReady = weapon.rate < fireDelay;
+
+        if (isFireReady == true)
+        {
+            weapon.Use();
+            animator.SetTrigger("Swing");
+            fireDelay = 0;
+        }
+    }
+}
